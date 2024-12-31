e2af345f70c94160ac03525910afc5b8c1db35a5
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-31 14:39:01 +0530

    create header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "../images/logo-2.png";
+import "../css/style.css";
+function Header() {
+  return (
+    <div className="container text-white header">
+      <div className="">
+        <img src={logo} className="logo mb-2 mt-4" alt="logo" height="70px" />
+
+        <h1 className="title ">Book Explorer</h1>
+
+
+        <div className="search-bar d-flex">
+           <input className="form-control " placeholder="Search your book"/>
+           <button className="btn btn-primary ms-2">
+           <i className="fas fa-search" />
+           </button>
+        </div>
+
+        
+      </div>
+    </div>
+  );
+}
+
+export default Header;
