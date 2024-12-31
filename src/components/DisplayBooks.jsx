8a53f7d19685fb972f81cefed762414437a9f6fc
AnjanaTharanga24 <anjana24myprof@gmail.comm>
2024-12-31 15:17:27 +0530

    add responsive design for the header section

--- src/components/DisplayBooks.jsx
+++ src/components/DisplayBooks.jsx
@@ -2,7 +2,45 @@ import React from 'react'
 
 function DisplayBooks() {
   return (
-    <div className='container book-container'>DisplayBooks</div>
+    <div className='container book-container'>
+        <div className='row'>
+            <div className='card book-card col-md-3'>
+              
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+
+            <div className='card book-card col-md-3'>
+
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+            <div className='card book-card col-md-3'>
+
+            </div>
+            
+            
+            
+
+        </div>
+    </div>
   )
 }
 
